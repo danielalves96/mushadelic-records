1470a13da22eff7f6f73da26e8c4843cf33fb69e
Daniel Alves <daniel.alves@gazin.com.br>
2022-06-02 16:45:12 -0300

    feat(app): create artists page

--- src/pages/artist/[slug].tsx
+++ src/pages/artist/[slug].tsx
@@ -1,10 +1,58 @@
 import React from 'react';
 import { db } from '@/firebase-config';
 import { collection, getDocs, query, where } from 'firebase/firestore';
+import {
+  SiSoundcloud,
+  SiYoutube,
+  SiSpotify,
+  SiFacebook,
+  SiInstagram,
+} from 'react-icons/si';
+import styles from './styles.module.scss';
 
 export default function Artist({ artistReturn }: any) {
   return (
     <>
+      <div className="columns">
+        <div className="column is-one-quarter">
+          <img src={artistReturn.picture} alt="" />
+        </div>
+        <div className="column">
+          <div className={styles.descriptionSection}>
+            <span className={styles.title}>
+              {artistReturn.artist_name.toUpperCase()}
+            </span>
+            <div className={styles.icons}>
+              <a
+                href={artistReturn.instgrm_link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SiInstagram size={25} />
+              </a>
+              <a href={artistReturn.fb_link} target="_blank" rel="noreferrer">
+                <SiFacebook size={25} />
+              </a>
+              <a href={artistReturn.sc_link} target="_blank" rel="noreferrer">
+                <SiSoundcloud size={25} />
+              </a>
+              <a
+                href={artistReturn.sptfy_link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SiSpotify size={25} />
+              </a>
+              <a href={artistReturn.yt_link} target="_blank" rel="noreferrer">
+                <SiYoutube size={25} />
+              </a>
+            </div>
+            <div className={styles.limitText}>
+              <span>{artistReturn.description}</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <div
         style={{ padding: `0.5rem` }}
         dangerouslySetInnerHTML={{
