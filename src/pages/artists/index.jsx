8e7381edae527f34e54a251b2ce9b42857b24613
Daniel Alves <daniel.xcoders@gmail.com>
2023-01-11 15:51:05 -0300

    fix: remove console

--- src/pages/artists/index.jsx
+++ src/pages/artists/index.jsx
@@ -24,7 +24,7 @@ export default function Home() {
   const [artistsMain, setArtistsMain] = useState([]);
   const artistsRef = collection(db, 'artists');
 
-  console.log(artistsMain);
+  // console.log(artistsMain);
 
   function orderArtists(a, b) {
     if (a.artist_number < b.artist_number) {
