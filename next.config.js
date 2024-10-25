036fb897a071e5842de15710102c35e3731eba73
Daniel Alves <daniel.xcoders@gmail.com>
2024-10-25 17:09:46 -0300

    chore: disable next caching

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store',
+        },
+      ],
+    },
+  ],
   images: {
     remotePatterns: [
       {
