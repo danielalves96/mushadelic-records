f13b64271c737ffed13629619ee477d6f7527309
Daniel Alves <daniel.xcoders@gmail.com>
2025-01-22 00:55:28 -0300

    feat: atualizar layout e metadados do festival Mushadelic

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,12 +1,32 @@
-export const metadata = {
-  title: 'Mushadelic Festival',
-  description: 'Winter',
+import { Metadata } from 'next';
+
+import '../globals.css';
+
+import { Inter } from 'next/font/google';
+import TanstackProvider from 'providers/tanstack-provider';
+import { ThemeProvider } from 'providers/theme-provider';
+
+import { ContentLayout } from '@/components/admin-panel/content-layout';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: `Mushadelic Records`,
+  description: `Psytrance Music Label`,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} bg-zinc-50 dark:bg-[#111318]`}>
+        <TanstackProvider>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            <main className="min-h-screen">
+              <ContentLayout>{children}</ContentLayout>
+            </main>
+          </ThemeProvider>
+        </TanstackProvider>
+      </body>
     </html>
   );
 }
